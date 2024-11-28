9e1888a87a75f1997202c9799b37fae3dabf3986
PaulLB <paul@leclercqbayle.com>
2024-11-28 20:35:58 +0100

    Ticket3

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
         <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) : ?>
             <a href="profil.php">Profil</a>
             <a href="my-events.php">Mes événements</a>
-            <a href="event-liste.php">Liste des événements</a>
+            <a href="event-list.php">Liste des événements</a>
             <a href="creation-event.php">Créer un événement</a>
             <a href="logout.php">Déconnexion</a>
         <?php else : ?>
